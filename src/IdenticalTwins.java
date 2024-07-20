a60d13ef4dac2e02bdc9624fa4df1f71016ea0c0
Amr Kahla <amrkahla6@gmail.com>
2024-07-20 13:08:08 +0300

    solve Identical Twins

---
+++ src/IdenticalTwins.java
@@ -0,0 +1,14 @@
+public class IdenticalTwins {
+    int getIdenticalTwinsCount (int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j= i+1; j <arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // CumulativeSum
 //       System.out.println(CumulativeSum.cumulativeSum(new int[]{1, 2, 3, 4}));
 //       System.out.println(CumulativeSum.cumulativeSum(new int[]{1, 1, 1, 1, 1}));
 //       System.out.println(CumulativeSum.cumulativeSum(new int[]{1, 3, 5, 7, 9}));
 
+        // PositiveCumulativeSum
 //        PositiveCumulativeSum positiveCumulativeSum = new PositiveCumulativeSum();
 //        System.out.println(positiveCumulativeSum.getPositiveCumulativeSum(new int[]{1, -2, 3, 4, -6}));
 //        System.out.println(positiveCumulativeSum.getPositiveCumulativeSum(new int[]{1, -1, -1, -1, 1}));
 //        System.out.println(positiveCumulativeSum.getPositiveCumulativeSum(new int[]{1, 3, 5, 7}));
 
-        
+        // IdenticalTwins
+         IdenticalTwins identicalTwins = new IdenticalTwins();
+         System.out.println(identicalTwins.getIdenticalTwinsCount(new int[]{1, 2, 3, 2, 1}));
+         System.out.println(identicalTwins.getIdenticalTwinsCount(new int[]{1, 2, 2, 3, 2, 1}));
+         System.out.println(identicalTwins.getIdenticalTwinsCount(new int[]{1, 1, 1, 1}));
+
     }
 }
